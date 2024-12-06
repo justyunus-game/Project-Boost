bb6c340302a9fe07c35874bcfbee63efcc2ce63e
Yunus Ali <justyunus.game@gmail.com>
2024-12-06 12:29:06 +0300

    Rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrustKey;
+    [SerializeField] InputAction rotationKey;
     [SerializeField] float thrustStrength = 20;
+    [SerializeField] float rotationStrength = 20;
 
     Rigidbody rb;
 
@@ -16,13 +18,38 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrustKey.Enable();
+        rotationKey.Enable();
     }
 
     private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrustKey.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotationKey.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
